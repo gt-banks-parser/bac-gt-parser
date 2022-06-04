6cf659453bfbd62c6914ca85415eb60bbb681293
Carlos Simon <git@csimon.dev>
2022-06-04 07:02:26 -0600

    fix fetch movements

--- src/bac_bank_gt/models.py
+++ src/bac_bank_gt/models.py
@@ -136,6 +136,11 @@ class BACBankAccount(AbstractBankAccount):
                 "initReference": None,
                 "endReference": None,
             },
+            headers={
+                "Origin": "https://www1.sucursalelectronica.com",
+                "Content-Type": "application/x-www-form-urlencoded",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+            },
         )
         logging.info(r)
         bs = BeautifulSoup(r, features="html5lib")
