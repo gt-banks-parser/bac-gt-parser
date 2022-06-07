1ece2b8d9f831eb191ae8c3b1f6362565b31f0ca
Carlos Simon <git@csimon.dev>
2022-06-07 05:02:47 -0600

    bump version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name="bac_bank_parser_gt",
-    version="0.2.0",
+    version="0.2.1",
     author="Carlos Simon",
     author_email="dev@csimon.dev",
     description="Banco America Central (BAC) Parser",
